fa1a6f893a3e213381b128cb71023139e7ae6b15
Carlos Toledo <carloseliastoledo@email.com>
2026-02-21 15:11:53 -0300

    Fix categories migration for sql_require_primary_key

--- prisma/migrations/20260125153528_add_categories/migration.sql
+++ prisma/migrations/20260125153528_add_categories/migration.sql
@@ -14,7 +14,8 @@ CREATE TABLE `_CategoriaToProduto` (
     `B` VARCHAR(191) NOT NULL,
 
     UNIQUE INDEX `_CategoriaToProduto_AB_unique`(`A`, `B`),
-    INDEX `_CategoriaToProduto_B_index`(`B`)
+    INDEX `_CategoriaToProduto_B_index`(`B`),
+    PRIMARY KEY (`A`, `B`)
 ) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
 
 -- AddForeignKey
