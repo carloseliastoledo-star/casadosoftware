12190f9a32e05aaa6036f57410189df2167ef1fb
Carlos Toledo <carloseliastoledo@gmail.com>
2026-05-19 15:04:09 -0300

    fix: filter admin categories by store using product association

--- server/api/admin/categorias/index.get.ts
+++ server/api/admin/categorias/index.get.ts
@@ -1,11 +1,26 @@
 import { defineEventHandler } from 'h3'
 import prisma from '../../../db/prisma'
 import { requireAdminSession } from '../../../utils/adminSession'
+import { getStoreContext } from '../../../utils/store'
 
 export default defineEventHandler(async (event) => {
   requireAdminSession(event)
 
+  const { storeSlug } = getStoreContext(event)
+
+  // Get categories that are used by products of the current store
   const categorias = await (prisma as any).Categoria.findMany({
+    where: storeSlug
+      ? {
+          ProdutoCategoria: {
+            some: {
+              Produto: {
+                storeSlug: storeSlug
+              }
+            }
+          }
+        }
+      : undefined,
     orderBy: { nome: 'asc' },
     select: {
       id: true,
