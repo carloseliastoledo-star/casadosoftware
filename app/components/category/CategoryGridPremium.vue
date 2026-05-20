f8ead91dd85d87b364adfa6cef82cd2efa92e2e3
Carlos Toledo <carloseliastoledo@gmail.com>
2026-05-20 14:27:07 -0300

    fix: increase grid columns from xl:grid-cols-4 to xl:grid-cols-5 in category grid to give more width to product cards - prevents image cropping due to sidebar

--- app/components/category/CategoryGridPremium.vue
+++ app/components/category/CategoryGridPremium.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="grid grid-cols-1 gap-5 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4">
+  <div class="grid grid-cols-1 gap-5 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-5">
     <slot />
   </div>
 </template>
