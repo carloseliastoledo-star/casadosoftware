7c286c99c02fdad073002679e935a82b1a632b07
Carlos Toledo <carloseliastoledo@gmail.com>
2026-05-31 18:56:38 -0300

    feat: re-enable static pages

--- app/utils/staticPagesFlags.ts
+++ app/utils/staticPagesFlags.ts
@@ -1,6 +1,6 @@
-export const STATIC_PAGES_ENABLED = false
-export const STATIC_HOME_ENABLED = false
-export const STATIC_OFFICE365_PRODUCT_ENABLED = false
+export const STATIC_PAGES_ENABLED = true
+export const STATIC_HOME_ENABLED = true
+export const STATIC_OFFICE365_PRODUCT_ENABLED = true
 
 export const OFFICE365_STATIC_SLUG =
   'microsoft-office-365-vitalicio-5-licencas-pc-mac-android-ou-ios-1-tb-one-drive'
